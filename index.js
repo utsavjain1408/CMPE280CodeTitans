be970f1376a8cd2e5cb4e6cb409ff62529e8a617
Utsav Jain <utsavjain1408@gmail.com>
2018-02-01 15:56:36 -0800

    Adding login and signup html files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
+var login = require('./src/controllers/controllers')
 const PORT = 3051
 
 var app = express()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.listen(PORT, () =>{
     console.log(`Titan Pizza Server is now serving at ${PORT}`)
 })
@@ -12,4 +14,6 @@ app.listen(PORT, () =>{
 app.get('/', (req, res)=>{
     res.send(`CODE TITAS FOR THE WIN!!`)
 })
+
+app.use('/home',login.login())
 
