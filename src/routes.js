8e06d798276c7430f2b71c3e0b086bcc76a50066
Utsav Jain <utsavjain1408@gmail.com>
2018-02-02 16:26:17 -0800

    very small bugs fix

--- src/routes.js
+++ src/routes.js
@@ -20,12 +20,12 @@ export const routes = (app) => {
         var controllers = require('./controllers/controllers');
         app.post('/',controllers.login)
         app.get('/', (req, res) => {
-            res.sendFile('\\view\\login.html',{"root": __dirname})
+            res.sendFile('/view/login.html',{"root": __dirname})
         })
         app.post('/signup', controllers.createAccout);
         app.get('/signup', function (req, res) {
             console.log('Under Construction!!');
-            res.sendFile('\\view\\signup.html',{"root": __dirname})
+            res.sendFile('/view/signup.html',{"root": __dirname})
 
         });
     };
