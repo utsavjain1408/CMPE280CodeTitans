030f195121ad7428b6f0f6c7a2f51a6ff7da1080
Utsav Jain <utsavjain1408@gmail.com>
2018-02-08 13:55:47 -0800

    Removed unnecessary code.

--- index.js
+++ index.js
@@ -12,23 +12,3 @@ app.listen(PORT, () =>{
     console.log(`Titan Pizza Server is now serving at ${PORT}`)
 })
 routes.route(app);
-
-app.use('/home',(req,res) =>{
-        res.sendFile('\\src\\view\\login.html',{"root": __dirname})
-})
-
-app.get('/test', (req,res)=>{
-    var html = '<!DOCTYPE html>\n'
-    + '<html lang="en-US">\n'
-    + '<head>\n'
-    + '    <meta charset="UTF-8">\n'
-    + '    <title>Hello, world</title>\n'
-    + '</head>\n'
-    + '<body>\n'
-    + '    <h1>Hello, world!</h1>\n'
-    + '</body>\n'
-    + '</html>\n';
-    res.send(html);
-})
-
-//app.get('/login', )
