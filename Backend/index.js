bb0cf061fca15618493d906e30aaf6dd2f77fe87
daisydanlu <daisydanlu@gmail.com>
2018-03-09 14:37:21 -0800

    Add cookie-parser middleware

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@
 
 var express = require('express')
 var bodyParser = require('body-parser')
+//var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var login = require('./src/controllers/controllers')
 var routes = require('./src/routes')
 const path = require('path')
@@ -13,6 +15,8 @@ app.set('views', path.join(__dirname, 'src/view'));
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+//app.use(cookieParser());
+app.use(session( {secret: "String for encrypting cookies." } ));
 app.listen(PORT, () =>{
     console.log(`Titan Pizza Server is now serving at ${PORT}`)
 })
